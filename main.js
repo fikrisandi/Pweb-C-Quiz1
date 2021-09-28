748eefafa48b68b872e268b26692ca58af79483e
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-09-28 22:42:41 +0700

    Push quiz1

---
+++ main.js
@@ -0,0 +1,73 @@
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
+const textDisplay = document.getElementById('text')
+const phrases = ['I Am a Student College', 'In Institute Technology Sepuluh Nopember']
+let i = 0
+let j = 0
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop() {
+    isEnd = false
+    textDisplay.innerHTML = currentPhrase.join('')
+
+    if (i < phrases.length) {
+
+        if (!isDeleting && j <= phrases[i].length) {
+            currentPhrase.push(phrases[i][j])
+            j++
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (isDeleting && j <= phrases[i].length) {
+            currentPhrase.pop(phrases[i][j])
+            j--
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (j == phrases[i].length) {
+            isEnd = true
+            isDeleting = true
+        }
+
+        if (isDeleting && j === 0) {
+            currentPhrase = []
+            isDeleting = false
+            i++
+            if (i === phrases.length) {
+                i = 0
+            }
+        }
+    }
+    const spedUp = Math.random() * (80 - 50) + 50
+    const normalSpeed = Math.random() * (300 - 200) + 200
+    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+    setTimeout(loop, time)
+}
+
+loop()
+
---
+++ profile/profile.js
@@ -0,0 +1,51 @@
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
+
+document.addEventListener("scroll", handleScroll);
+// get a reference to our predefined button
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+
+function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.5;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+    //show button
+    scrollToTopBtn.style.display = "block";
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
